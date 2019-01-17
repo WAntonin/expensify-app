060678b6d782d646f70618f13004ddf2fea6a18c
Antonin Welvart <a.welvart@gmail.com>
2019-01-17 14:14:59 +0000

    Add style

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import selectExpenses from '../selectors/expenses'
 import selectTotalExpenses from '../selectors/expenses-total'
 import numeral from 'numeral'
 
 
-export const ExpensesSummary = ({expenseCount, expenseTotal}) => {
+export const ExpensesSummary = ({ expenseCount, expenseTotal }) => {
     const plural = expenseCount > 1 ? 's' : ''
     const formattedExpenseTotal = numeral(expenseTotal / 100).format('0,0.00$')
-    return(
-    <div>
-        <h1>Viewing {expenseCount} expense{plural} for a total of {formattedExpenseTotal}</h1>
-    </div>
+    return (
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> expense{plural} for a total of <span>{formattedExpenseTotal}</span></h1>
+            <div className="page-header__actions">
+            <Link className="button" to="/create">Add expense</Link>
+            </div>
+                </div>
+        </div>
     )
 }
 
